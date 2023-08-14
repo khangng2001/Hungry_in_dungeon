38334ad0901348a5c36cf8f9240141630e02b562
khangng2001 <72006373+khangng2001@users.noreply.github.com>
2023-08-14 11:29:02 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 5f;
+    private Vector3 moveDir = Vector3.zero;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+    }
+
+    private void Moving()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (horizontal < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        
+        moveDir.Set(horizontal, vertical, 0f);
+        moveDir.Normalize();
+        transform.Translate( moveDir * (moveSpeed * Time.deltaTime));
+    }
+}
