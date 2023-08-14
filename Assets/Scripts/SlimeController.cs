4c1b4411e44f8bedb1b958e4178646221186a8fc
ngocthinhh <ngcthnh2@gmail.com>
2023-08-14 15:41:50 +0700

    Update Demo Enemy script

---
+++ Assets/Scripts/SlimeController.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeController : EnemyController
+{
+    public void StateInProgress()
+    {
+        switch (currentState)
+        {
+            case State.NonDetech:
+
+                break;
+            case State.Detech:
+
+                break;
+        }
+    }
+
+    public void SwitchState(State newState)
+    {
+        switch (currentState)
+        {
+            case State.NonDetech:
+
+                break;
+            case State.Detech:
+
+                break;
+        }
+
+        switch (newState)
+        {
+            case State.NonDetech:
+
+                break;
+            case State.Detech:
+
+                break;
+        }
+
+        currentState = newState;
+    }
+
+    public enum StateInDetech
+    {
+        Idle,
+        Run,
+        Attack,
+        Hurt,
+        Die
+    }
+    public StateInDetech currentStateInDetech;
+
+    public void StateInDetechInProgress()
+    {
+        switch (currentStateInDetech)
+        {
+            case StateInDetech.Idle:
+
+                break;
+            case StateInDetech.Run:
+
+                break;
+            case StateInDetech.Attack:
+
+                break;
+            case StateInDetech.Hurt:
+
+                break;
+            case StateInDetech.Die:
+
+                break;
+        }
+    }
+
+    public void SwitchStateInDetch(StateInDetech newStateInDetech)
+    {
+        switch (currentStateInDetech)
+        {
+            case StateInDetech.Idle:
+
+                break;
+            case StateInDetech.Run:
+
+                break;
+            case StateInDetech.Attack:
+
+                break;
+            case StateInDetech.Hurt:
+
+                break;
+            case StateInDetech.Die:
+
+                break;
+        }
+
+        switch (newStateInDetech)
+        {
+            case StateInDetech.Idle:
+
+                break;
+            case StateInDetech.Run:
+
+                break;
+            case StateInDetech.Attack:
+
+                break;
+            case StateInDetech.Hurt:
+
+                break;
+            case StateInDetech.Die:
+
+                break;
+        }
+
+        currentStateInDetech = newStateInDetech;
+    }
+
+    private void Awake()
+    {
+        SetUpMaxHealth(100f);
+        SetSpeed(5f);
+        SetStrength(10f);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        StateInProgress();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
