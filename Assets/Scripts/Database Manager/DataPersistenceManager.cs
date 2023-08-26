a07c7e3a6afa1dc21e435e3bcb38e6b357aca825
TuanMinh1011 <tuanpm1011@gmail.com>
2023-08-26 21:13:53 +0700

    Restore done

--- Assets/Scripts/Database Manager/DataPersistenceManager.cs
+++ Assets/Scripts/Database Manager/DataPersistenceManager.cs
@@ -1,12 +1,20 @@
+using Realms.Sync;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DataPersistenceManager : MonoBehaviour
 {
+    MongoClient.Collection<GameData> collection;
+
+    private void FindId()
+    {
+        
+    }
+
     private void NewGame()
     {
-
+        
     }
 
     private void LoadGame()
