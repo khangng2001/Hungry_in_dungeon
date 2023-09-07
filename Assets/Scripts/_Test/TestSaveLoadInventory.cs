252dde8331abc47beec9cbcea19dc53e3703be0e
TuanMinh1011 <tuanpm1011@gmail.com>
2023-09-07 18:31:36 +0700

    Update NPM and Save and Load Inventory

--- Assets/Scripts/_Test/TestSaveLoadInventory.cs
+++ Assets/Scripts/_Test/TestSaveLoadInventory.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class TestSaveLoadInventory : MonoBehaviour
+public class TestSaveLoadInventory : MonoBehaviour, IDataPersistence
 {
     public InventorySlot[] inventorySlots; 
     public ItemSO[] item;
@@ -97,5 +98,53 @@ public class TestSaveLoadInventory : MonoBehaviour
                 }
             }
         }*/
+    }
+
+
+
+    //===================LOAD AND SAVE DATA=============================
+    public void LoadData(GameData data)
+    {
+        //ClearAllItem();
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventoryItem itemInSlot = inventorySlots[i].GetComponentInChildren<InventoryItem>();
+            for (int j = 0; j < item.Length; j++)
+            {
+                if (itemName[j] == data.inventory[i].Name && itemInSlot == null)
+                {
+                    InventoryManager.instance.LoadSpawnItem(item[j], inventorySlots[i], data.inventory[i].Count);
+                }
+            }
+        }
+        //for (int i = 0; i < inventorySlots.Length; i++)
+        //{
+        //    InventoryItem itemInSlot = inventorySlots[data.inventory[i].Slot].GetComponentInChildren<InventoryItem>();
+        //    if (itemName[i] == data.inventory[i].Name)
+        //    {
+        //        InventoryManager.instance.LoadData(i, inventorySlots[data.inventory[i].Slot], data.inventory[i].Count);
+        //    }
+        //}
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot != null)
+            {
+                Debug.Log("i: " + i + ",name: " + itemInSlot.item.id + ",count: " + itemInSlot.count);
+                data.inventory[i].Name = itemInSlot.item.id;
+                data.inventory[i].Count = itemInSlot.count;
+                data.inventory[i].Slot = i;
+            } else
+            {
+                data.inventory[i].Name = null;
+                data.inventory[i].Count = 0;
+                data.inventory[i].Slot = 0;
+            }
+        }
     }
 }
